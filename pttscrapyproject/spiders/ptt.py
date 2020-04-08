6691c0d41edb02653f2a5353bbda6588e2dbb1ab
god-of-thunder <52755752+god-of-thunder@users.noreply.github.com>
2020-04-08 15:49:37 +0800

    Update ptt.py

--- pttscrapyproject/spiders/ptt.py
+++ pttscrapyproject/spiders/ptt.py
@@ -124,9 +124,9 @@ class PttSpider(scrapy.Spider):
 
                     comment_time_list[0] = comment_time_list[0][-1].replace("\\n'>", '')
 
-                comment_time_list[0] = comment_time_list[0] + ' ' + time.ctime(time.time()).split(' ')[4]
+                comment_time_list[0] = comment_time_list[0].strip() + ' ' + time.ctime(time.time()).split(' ')[5]
 
-                item['comment_time'] = round(time.mktime(time.strptime(comment_time_list[0], " %m/%d %H:%M %Y")))
+                item['comment_time'] = round(time.mktime(time.strptime(comment_time_list[0], "%m/%d %H:%M %Y")))
 
             except ValueError:
 
@@ -140,7 +140,7 @@ class PttSpider(scrapy.Spider):
 
                     comment_time = comment_time.join(comment_time_list[f])
 
-                    comment_time = comment_time + ' ' + time.ctime(time.time()).split(' ')[4]
+                    comment_time = comment_time + ' ' + time.ctime(time.time()).split(' ')[5]
 
                 item['comment_time'] = round(time.mktime(time.strptime(comment_time, "%m/%d %H:%M %Y")))
 
